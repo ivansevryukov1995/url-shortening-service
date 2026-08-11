d2d2446ea029a49df317892c3b053a9f28243ac3
Ivan Sevryukov <sevryukovivan1995@mail.ru>
2026-08-11 13:06:10 +0300

    feat: add simple response

--- intertnal/auth/handler.go
+++ intertnal/auth/handler.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"encoding/json"
 	"log/slog"
 	"net/http"
 
@@ -26,6 +27,13 @@ func NewAuthHandler(router *http.ServeMux, deps AuthHandlerDeps) {
 func (handler *AuthHandler) Login() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		slog.Info("Login")
+		res := LoginResponse{
+			Token: "123",
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(res)
 	}
 }
 func (handler *AuthHandler) Register() http.HandlerFunc {
