aa82f3a5b7b6da486b26623d7b2f7c5cc7f51095
Ivan Sevryukov <sevryukovivan1995@mail.ru>
2026-08-24 17:13:54 +0300

    feat: add TestRegisterSuccess

---
+++ intertnal/auth/service_test.go
@@ -0,0 +1,46 @@
+package auth_test
+
+import (
+	"testing"
+
+	"github.com/ivansevryukov1995/url-shortening-service/intertnal/auth"
+	"github.com/ivansevryukov1995/url-shortening-service/intertnal/model"
+)
+
+type MockUserRepository struct {
+}
+
+func (repo *MockUserRepository) Create(user *model.User) (*model.User, error) {
+	return &model.User{
+		Email:    "a@a.ru",
+		Password: "1",
+		Name:     "John",
+	}, nil
+}
+
+func (repo *MockUserRepository) FindByEmail(email string) (*model.User, error) {
+	return nil, nil
+}
+
+func TestRegisterSuccess(t *testing.T) {
+	param := struct {
+		email string
+		pass  string
+		name  string
+	}{
+		email: "a@a.ru",
+		pass:  "1",
+		name:  "John",
+	}
+
+	authService := auth.NewAuthService(&MockUserRepository{})
+
+	email, err := authService.Register(param.email, param.pass, param.name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if email != param.email {
+		t.Fatalf("Email %s not equal %s", param.email, email)
+	}
+
+}
